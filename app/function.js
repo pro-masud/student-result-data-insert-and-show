8168cc018928fabbec62f78c1e57ab928ddbf5af
pro-mannan <promasudbd@gmail.com>
2023-08-12 01:20:14 +0600

    complete this basic project

--- app/function.js
+++ app/function.js
@@ -1,7 +1,7 @@
 // create a alertStatus arrow function 
 
 const alertStatus = (status, key = 'danger') => {
-    return `<div class="alert alert-${key} alert-dismissible fade show" role="alert">${status}
+    return `<div class="alert alert-${key} alert-dismissible fade show mt-2" role="alert">${status}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>`;
 }
@@ -25,4 +25,43 @@ const isEmail = (email) => {
     const pattern = /^[a-z]{2,}@[a-z]{3,}\.[a-z]{3,5}$/;
 
     return pattern.test(email);
+}
+
+/**
+ * gpc and gread create this function here
+ * 
+ * */ 
+
+const getResults = (marks) => {
+
+    let gpa;
+    let grade;
+
+    if( marks >= 0 && marks <= 32 ){
+        gpa = 0 ;
+        grade = "F";
+    }else if ( marks >= 33 && marks < 40 ){
+        gpa = 1;
+        grade = "D";
+    }else if ( marks >= 40 && marks < 50 ){
+        gpa = 2;
+        grade = "C";
+    }else if ( marks >= 50 && marks < 60 ){
+        gpa = 3;
+        grade = "B";
+    }else if( marks >= 60 && marks < 70){
+        gpa = 3.5;
+        grade = "A-";
+    }else if( marks >= 70 && marks < 80 ){
+        gpa = 4;
+        grade = "A";
+    }else if( marks >= 80 && marks <= 100){
+        gpa = 5;
+        grade = "A+";
+    }else{
+        gpa = "Invalide Data";
+        grade = "Invalide Data";
+    }
+
+    return {gpa,grade};
 }
--- app/main.js
+++ app/main.js
@@ -1,1 +1,59 @@
-// get data here
+// get form form data here
+
+const form_Data = document.getElementById("formData");
+const msg = document.querySelector(".msg");
+const loaderImg = document.querySelector(".loader-img");
+const clickBtn = document.querySelector(".click");
+const faudio = document.querySelector(".faudio");
+const haudio = document.querySelector(".haudio");
+
+form_Data.onsubmit = (e) => {
+  e.preventDefault();
+
+  // play for submit saunt
+  clickBtn.play();
+
+  const data_form = new FormData(e.target);
+
+  const data = Object.fromEntries(data_form.entries());
+
+
+  loaderImg.style.display = "block";
+
+  setTimeout(() => {
+
+    // Show Image Loader
+    loaderImg.style.display = "none";
+
+
+    // data validation here
+    if(!data.subject || !data.mark){
+      msg.innerHTML = alertStatus("All Field Required !!");
+    }else{
+      // get result for getResults function 
+      const {gpa, grade} = getResults(data.mark);
+
+      // condition to Pass and Field 
+      if(grade === "F"){
+        faudio.play();
+      }else{
+        haudio.play();
+      }
+
+      // show output data here
+      msg.innerHTML = alertStatus(`Subject: ${data.subject} / GPA ${gpa} / Grade ${grade}
+      `,'info');
+    }
+
+
+    // stop sount
+    clickBtn.stop(3000);
+  }, 3000);
+
+
+
+  // form reset 
+
+  e.target.reset();
+
+}
